e42fd4c227ddd5cbafa95b12e8a9d7660c658dfb
Aadit <baggaaadit017@gmail.com>
2021-07-02 10:02:08 +0530

    done ig

---
+++ encapsulationS.java
@@ -0,0 +1,7 @@
+package FOR_SCOOL;
+public class encapsulationS{ 
+    public static void main(String args[]){ 
+        private String not_to_be_shared = "this value shldnt be shared ";
+        // ^ this can be an example of encapsulation [ not showing the irrelevant information]  
+    }
+}
